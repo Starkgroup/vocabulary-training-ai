8470dc50f159d351ecc5b24e5e44d541172e6c1c
Starkgroup <org@starkgroup.de>
2024-11-04 15:58:27 +0100

    CORS fix

--- pages/api/request-token.js
+++ pages/api/request-token.js
@@ -5,13 +5,17 @@ import jwt from 'jsonwebtoken';
 export default function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method Not Allowed' });
-    }
+    } 
 
     const expirationInSeconds = 2592000;
-    const password = req.body.password; // Get the password from the request body
-    if (password !== process.env.PASSWORD) {
+    const password = req.body.password;
+
+    if (password === undefined) {
+        return
+    } else if (password !== process.env.PASSWORD) {
         return res.status(401).json({ message: 'Unauthorized' });
-    }
+    } 
+
     const payload = {
         exp: Math.floor(Date.now() / 1000) + expirationInSeconds,
         // You can add more claims here if needed
